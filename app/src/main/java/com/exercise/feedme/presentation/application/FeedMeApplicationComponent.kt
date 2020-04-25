09f3975c9a15ff65f5b09958ce0c57a2f3b1545b
Felipe Zadinello <felipe.zadinello@arctouch.com>
2020-04-25 18:54:57 -0300

    Add Singleton to component

--- app/src/main/java/com/exercise/feedme/presentation/application/FeedMeApplicationComponent.kt
+++ app/src/main/java/com/exercise/feedme/presentation/application/FeedMeApplicationComponent.kt
@@ -3,12 +3,13 @@ package com.exercise.feedme.presentation.application
 import android.content.Context
 import com.exercise.feedme.data.remote.NetworkModule
 import com.exercise.feedme.domain.nutrition.NutritionModule
-import com.exercise.feedme.presentation.main.ActivityMainModule
 import dagger.BindsInstance
 import dagger.Component
 import dagger.android.AndroidInjectionModule
 import dagger.android.AndroidInjector
+import javax.inject.Singleton
 
+@Singleton
 @Component(modules = [AndroidInjectionModule::class, NetworkModule::class, NutritionModule::class])
 interface FeedMeApplicationComponent : AndroidInjector<FeedMeApplication> {
 
